b255fd4b7117a1b7ba87ab313acd44fb0f09e260
emilio richter <emiliorg@me.com>
2021-09-02 07:53:02 -0600

    Create main.js

---
+++ Actividades de algoritmos/js-1/main.js
@@ -0,0 +1,44 @@
+/*
+const bebio_cafe = false;
+const hay_cafe = true;
+
+if(bebio_cafe === true) {
+    console.log("el usuario bebio cafe")
+} else if {
+    console.log("el usuario preparara cafe")
+} else {
+    console.log("el usuario no bebio cafe")
+}
+*/
+
+/*
+const miEdad = 21;
+const tuEdad = 30;
+
+if(miEdad !== tuEdad){
+    console.log("tenemos diferente edad")
+} else {
+    console.log("tenemos la misma edad")
+}
+*/
+
+/*
+for(let i = 0; i < 10; i++){
+    console.log(i)
+}
+*/
+
+/*
+let myArray = ['leon', 'jirafa', 'lagartija', 'perro'];
+
+
+for(let i = 0; i <= myArray.length; i++){
+    console.log("jaime esta con su" + "" + myArray[i])
+}
+*/
+
+/*
+myArray.forEach(animal =>{
+    console.log("javier esta con su" + animal)
+})
+*/
